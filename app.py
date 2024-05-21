a74c7c066d89a532e4ebb768a29e5687d203e0bd
Rithvik <nrithvik19@gmail.com>
2024-05-21 11:46:43 +0530

    resolved branch conflit

--- app.py
+++ app.py
@@ -1,58 +1,22 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request
-=======
 from flask import Flask, render_template, request, jsonify
->>>>>>> dev
 import pandas as pd
 import numpy as np
 from src.mlProject.pipeline.prediction import PredictionPipeline
 import warnings
-<<<<<<< HEAD
-=======
 import joblib
 from pathlib import Path
->>>>>>> dev
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-=======
 model = joblib.load(Path("artifacts/model_trainer/model.joblib"))
 
->>>>>>> dev
 @app.route('/', methods=['GET'])  
 def homePage():
     return render_template("index.html")
 
-<<<<<<< HEAD
-
-@app.route('/predict', methods=['POST', 'GET']) 
-def index():
-    if request.method == 'POST':
-        try:
-            # Define the feature names
-            feature_names = [
-                'product_category_name', 'qty', 'freight_price', 'unit_price', 'product_score', 
-                'customers', 'weekend', 'holiday', 'month', 'volume', 'comp_1', 'ps1', 'fp1', 
-                'comp_2', 'ps2', 'fp2', 'comp_3', 'ps3', 'fp3'
-            ]
-            
-            input_data = []
-            for feature in feature_names:
-                input_data.append(request.form[feature])
-                
-            input_data_df = pd.DataFrame([input_data], columns=feature_names)
-
-            # Initialize an instance of the PredictionPipeline class
-            obj = PredictionPipeline('artifacts/model_trainer/model.joblib')
-
-            # Use the predict method to make predictions
-            predict = obj.predict(input_data_df)
-
-            return render_template('results.html', prediction=predict[0])
-=======
 @app.route('/predict', methods=['POST', 'GET']) 
 def web_predict():
     if request.method == 'POST':
@@ -93,7 +57,7 @@ def web_predict():
             predict = obj.predict(input_data)
 
             return render_template('results.html', prediction=predict)
->>>>>>> dev
+
         
         except Exception as e:
             print('The Exception message is: ', e)
@@ -102,8 +66,7 @@ def web_predict():
     else:
         return render_template('index.html')
 
-<<<<<<< HEAD
-=======
+
 @app.route('/api/predict', methods=['POST']) 
 def api_predict():
     try:
@@ -154,6 +117,5 @@ def api_predict():
         print('The Exception message is:', e)
         return 'Something went wrong', 400
 
->>>>>>> dev
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- src/mlProject/pipeline/prediction.py
+++ src/mlProject/pipeline/prediction.py
@@ -5,28 +5,12 @@ from pathlib import Path
 from sklearn.preprocessing import LabelEncoder
 
 class PredictionPipeline:
-<<<<<<< HEAD
-    def __init__(self, model_path):
-        self.model = joblib.load(Path(model_path))
-=======
     def __init__(self):
         self.model = joblib.load(Path("artifacts/model_trainer/model.joblib"))
->>>>>>> dev
         self.label_encoders = {}  # Store label encoders for categorical variables
 
     def preprocess_data(self, data):
         # Convert data to DataFrame
-<<<<<<< HEAD
-        df = pd.DataFrame(data)
-
-        # Preprocess categorical variables
-        for col_index, col_name in enumerate(df.columns):
-            if df.dtypes[col_index] == 'object':
-                # If the column is categorical, encode it using LabelEncoder
-                if col_name not in self.label_encoders:
-                    self.label_encoders[col_name] = LabelEncoder()
-                df[col_name] = self.label_encoders[col_name].fit_transform(df[col_name])
-=======
         model_features = list(self.model.feature_names_in_)
 
         df = pd.DataFrame([data], columns=model_features)
@@ -40,7 +24,6 @@ class PredictionPipeline:
                     unique_categories = ['bed_bath_table', 'garden_tools', 'consoles_games','health_beauty', 'cool_stuff', 'perfumery','computers_accessories', 'watches_gifts', 'furniture_decor'] 
                     self.label_encoders[col_name].fit(unique_categories)
                 df[col_name] = self.label_encoders[col_name].transform(df[col_name])
->>>>>>> dev
 
         return df
 
@@ -48,8 +31,4 @@ class PredictionPipeline:
         preprocessed_data = self.preprocess_data(data)
         prediction = self.model.predict(preprocessed_data)
 
-<<<<<<< HEAD
-        return prediction
-=======
         return prediction[0]
->>>>>>> dev
